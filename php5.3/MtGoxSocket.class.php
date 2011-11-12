b9fb58139e5f61c7953be3ef049a23e199ea3f91
Mark Karpeles <mark@hell.ne.jp>
2011-11-12 11:56:49 +0900

    new mtgox api via websocket

---
+++ php5.3/MtGoxSocket.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class MtGoxSocket extends SocketIO {
+	private $api = null; // api access
+
+	public function __construct($key, $secret) {
+		$this->api = array($key, $secret);
+		$this->on('message', array($this, 'handleMessage'));
+		parent::__construct('https://socketio.mtgox.com/mtgox');
+	}
+
+	public function handleMessage($msg) {
+		if (is_string($msg)) $msg = json_decode($msg, true);
+		var_dump($msg);
+	}
+
+	public function call($call, $params = array(), $item = null, $currency = null) {
+		$nonce = explode(' ', microtime(false));
+		$nonce = $nonce[1].substr($nonce[0], 2, 6);
+		$id = md5($nonce); // id can be anything to recognize this call
+		$query = array('call' => $call, 'params' => $params, 'item' => $item, 'currency' => $currency, 'id' => $id, 'nonce' => $nonce);
+		$query = json_encode($query);
+		// generate signature
+		$sign = hash_hmac('sha512', $query, base64_decode($this->api[1]), true);
+		// prefix signature to query
+		$query = pack('H*', str_replace('-','',$this->api[0])).$sign.$query;
+		// send query
+		$call = array('op' => 'call', 'call' => base64_encode($query), 'id' => $id, 'context' => 'mtgox.com');
+		$this->send_json($call);
+		return $id;
+	}
+}
+
